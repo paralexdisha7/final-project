6f9013f51572025f0c1fd97825b23e2c6029730e
Disha Parale <paraledisha20@gmail.com>
2023-09-28 00:00:58 +0530

    AddPlace, form ,input

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,9 +1,35 @@
-import React from 'react'
-
+import React, { useCallback } from "react";
+import "./NewPlace.css";
+import Input from "../../shared/components/FormElements/Input";
+import {
+  VALIDATOR_MINLENGTH,
+  VALIDATOR_REQUIRE,
+} from "../../shared/util/validators";
 const NewPlace = () => {
+  const titleInputHandler = useCallback((id, value, isValid) => {}, []);
+  const descriptionInputHandler = useCallback((id, value, isValid) => {}, []);
   return (
-    <div>NewPlace</div>
-  )
-}
+    <div className="place-form">
+      <Input
+        id="title"
+        element="input"
+        type="text"
+        label="Title"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Enter a Valid Title "
+        onInput={titleInputHandler}
+      />
+      <Input
+        id="description"
+        element="textarea"
+        type="text"
+        label="Descriptiom"
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        errorText="Enter a Valid Description. "
+        onInput={descriptionInputHandler}
+      />
+    </div>
+  );
+};
 
-export default NewPlace
+export default NewPlace;
